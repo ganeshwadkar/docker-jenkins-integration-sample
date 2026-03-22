87f37d1dd8663d54e40f18bbbd2801a1d210322d
Ganesh Wadkar <ganesh.wadkar@gmail.com>
2026-03-23 00:19:04 +0530

    added dockerfile

--- src/main/java/com/javatechie/dockerjenkinsintegrationsample/DockerJenkinsIntegrationSampleApplication.java
+++ src/main/java/com/javatechie/dockerjenkinsintegrationsample/DockerJenkinsIntegrationSampleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DockerJenkinsIntegrationSampleApplication {
 
     public static void main(String[] args) {
+        System.out.println("Test message");
         SpringApplication.run(DockerJenkinsIntegrationSampleApplication.class, args);
     }
 
