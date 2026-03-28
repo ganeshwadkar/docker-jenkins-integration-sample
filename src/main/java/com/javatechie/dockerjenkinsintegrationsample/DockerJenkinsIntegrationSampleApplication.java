820276d7be14e0faddb9961ef886f383d823e2e1
Ganesh Wadkar <ganesh.wadkar@gmail.com>
2026-03-28 23:50:12 +0530

    self-hosted config added for kind

--- src/main/java/com/javatechie/dockerjenkinsintegrationsample/DockerJenkinsIntegrationSampleApplication.java
+++ src/main/java/com/javatechie/dockerjenkinsintegrationsample/DockerJenkinsIntegrationSampleApplication.java
@@ -2,7 +2,10 @@ package com.javatechie.dockerjenkinsintegrationsample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DockerJenkinsIntegrationSampleApplication {
 
@@ -11,8 +14,10 @@ public class DockerJenkinsIntegrationSampleApplication {
         SpringApplication.run(DockerJenkinsIntegrationSampleApplication.class, args);
     }
 
-    public void hello(){
-        System.out.println("This is hello method");
+    @GetMapping("/hello")
+    public String hello() {
+        System.out.println("Hello API called successfully ...");
+        return "This is hello method v0.1";
     }
 
 }
